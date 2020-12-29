001fe5a54d2c04f09cb559d2b02bffe20aeb0e26
mbmosman <mbMosman@users.noreply.github.com>
2020-12-29 13:04:13 -0600

    updates for hooks

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,19 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
+function App() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Gallery of my life</h1>
+          <h1 className="App-title">Gallery of My Life</h1>
         </header>
-        <br/>
         <p>Gallery goes here</p>
         <img src="images/goat_small.jpg"/>
       </div>
     );
-  }
 }
 
 export default App;
